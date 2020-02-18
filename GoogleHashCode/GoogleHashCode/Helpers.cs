168dc6fd853f8f29da152c4bc84b4ed04efec70c
Altamish Mahomed <gambit318@gmail.com>
2020-02-18 16:59:48 +0200

    Restructured whole project, added generics, and solved practice problem.

---
+++ GoogleHashCode/GoogleHashCode/Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleHashCode
+{
+    class Helpers
+    {
+        public static Dictionary<string, string> ListToDictionary(List<string> list, string keyValDelimiter)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            if (keyValDelimiter == null || keyValDelimiter.Length == 0)
+            {
+                keyValDelimiter = Defaults.DELIMITER_KEYVAL;
+            }
+
+            foreach (string line in list)
+            {
+                int delimIndex = line.IndexOf(keyValDelimiter);
+                string key = line.Substring(0, delimIndex);
+                string value = line.Substring(delimIndex + keyValDelimiter.Length);
+                dictionary.Add(key, value);
+            }
+
+            return dictionary;
+        }
+
+        public static List<T> StringToList<T>(string input, string delimiter = Defaults.DELIMITER_SPACE)
+        {
+            //return input.Split(delimiter).ToList<T>();
+
+            List<string> l = input.Split(delimiter).ToList();
+
+            List<T> list = new List<T>();
+            foreach (string s in l)
+            {
+                list.Add(TConverter.ChangeType<T>(s));
+            }
+            return list;
+        }
+    }
+}
